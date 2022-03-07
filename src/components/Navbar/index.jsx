564ce59c1fb35d7d194bf5e7e6a1e909f3b29f6b
viettu2k <doanviettu2k@gmail.com>
2022-03-07 09:17:12 +0700

    adding navbar jsx

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
-      <h1>Navbar</h1>
+      <Nav>
+        <NavLink to="/">
+          <img src="" alt="" />
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/services" activeStyle>
+            Services
+          </NavLink>
+          <NavLink to="/contact" activeStyle>
+            Contact Us
+          </NavLink>
+          <NavLink to="/sign-up" activeStyle>
+            Sign Up
+          </NavLink>
+          <NavBtn>
+            <NavBtnLink to="/signin" />
+          </NavBtn>
+        </NavMenu>
+      </Nav>
     </div>
   );
 };
